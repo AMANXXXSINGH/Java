db03cefbbf3e64bbaac44fb88e66b1237f7c7208
AMANXXXSINGH <aman9958971302@gmail.com>
2025-07-14 02:07:46 +0530

    Stack

---
+++ Crux_Np_March_25/src/Lec25/Construct_Smallest_Number_From_DI_String.java
@@ -0,0 +1,14 @@
+package Lec25;
+
+public class Construct_Smallest_Number_From_DI_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str= "IIIDIDDD";
+System.out.println(C);
+	}
+public static String Construct_Smallest_Numbers(String s) {
+	int [] ans=new int[s.length()+1];
+
+}
+}
